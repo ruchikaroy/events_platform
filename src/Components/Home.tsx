ce6dcc72bce015904e27903de59d63c0e196681d
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-10 07:06:14 +0000

    changes made to redirectTo

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -31,7 +31,7 @@ const Home = () => {
         provider: "google",
         options: {
           scopes: "https://www.googleapis.com/auth/calendar",
-          redirectTo: "https://rr-events-biz.netlify.app/eventslist",
+          redirectTo: "http://localhost:3000/eventslist",
         },
       })
       .then((data) => {
