8974aabdbff32c748d62b5f453261fe68da771b3
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-10 07:27:17 +0000

    changes made to redirectTo-change version 3

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -31,7 +31,7 @@ const Home = () => {
         provider: "google",
         options: {
           scopes: "https://www.googleapis.com/auth/calendar",
-          redirectTo: "https://rr-events-biz.netlify.app/",
+          redirectTo: window.location.href,
         },
       })
       .then((data) => {
