46faabbb8c8616f352acac6bddd2be318c243285
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-10 08:25:14 +0000

    changes made to redirectTo-change version 7 - my domain added with auth/callback

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -31,7 +31,7 @@ const Home = () => {
         provider: "google",
         options: {
           scopes: "https://www.googleapis.com/auth/calendar",
-          redirectTo: "http://localhost:300/auth/callback",
+          redirectTo: "https://rr-events-biz.netlify.app/auth/callback",
           queryParams: {
             access_type: "offline",
             prompt: "consent",
