3cf22928da59f7a9d9e9537592f188c66a50572e
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-10 16:45:53 +0000

    maps event to add to google calendar url

--- src/Components/EventCard.tsx
+++ src/Components/EventCard.tsx
@@ -199,7 +199,7 @@ const EventCard = ({ showActions, isAdmin }: Props) => {
                           Register
                         </button>
                         <a
-                          href="https://calendar.google.com/calendar/u/0/r/eventedit"
+                          href={`https://calendar.google.com/calendar/u/0/r/eventedit?text=${event.name.text}&dates=${event.start.local}/${event.end.local}&details=${event.description.text}`}
                           target="_blank"
                           rel="noopener noreferrer"
                         >
