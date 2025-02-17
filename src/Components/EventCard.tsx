aaa82b48034444f5b409335f650948958e64bb5c
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-17 15:57:01 +0000

    adds create event card on the top

--- src/Components/EventCard.tsx
+++ src/Components/EventCard.tsx
@@ -124,6 +124,22 @@ const EventCard = ({ showActions, isAdmin }: Props) => {
         ) : (
           <ul>
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {isAdmin && (
+                <div className="flex flex-col items-center justify-center bg-white shadow-md border rounded-lg w-full sm:w-80 mx-auto p-6 min-h-[550px]">
+                  {/* // <div className="flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96  justify-center items-center"> */}
+                  <h6>
+                    <li>
+                      <button
+                        className="rounded-md bg-slate-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button"
+                        onClick={handleCreateEventButtonClick}
+                      >
+                        Create Event
+                      </button>
+                    </li>
+                  </h6>
+                </div>
+              )}
               {eventData.map((event) => (
                 <div
                   key={event.id}
