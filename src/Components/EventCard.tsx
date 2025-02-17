7991118623a8f04f3ce7c749c680d762b6c842f6
ruchikaroy <contactrroy.uk@gmail.com>
2025-02-17 16:02:18 +0000

    adjusts margin of the event card component

--- src/Components/EventCard.tsx
+++ src/Components/EventCard.tsx
@@ -110,7 +110,7 @@ const EventCard = ({ showActions, isAdmin }: Props) => {
 
   return (
     <>
-      <div style={{ margin: "20px" }}>
+      <div className="m-1">
         {isLoading ? (
           <div className="d-flex justify-content-center align-items-center mt-4">
             <Spinner animation="border" variant="white" role="status">
@@ -240,22 +240,6 @@ const EventCard = ({ showActions, isAdmin }: Props) => {
                   </div>
                 </div>
               ))}
-              {isAdmin && (
-                <div className="flex flex-col items-center justify-center bg-white shadow-md border rounded-lg w-full sm:w-80 mx-auto p-6 min-h-[550px]">
-                  {/* // <div className="flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96  justify-center items-center"> */}
-                  <h6>
-                    <li>
-                      <button
-                        className="rounded-md bg-slate-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        type="button"
-                        onClick={handleCreateEventButtonClick}
-                      >
-                        Create Event
-                      </button>
-                    </li>
-                  </h6>
-                </div>
-              )}
             </div>
           </ul>
         )}
