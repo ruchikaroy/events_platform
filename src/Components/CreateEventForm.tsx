54be1ea67e7f70994cc2fdc7e8083590db04d3c4
ruchikaroy <contactrroy.uk@gmail.com>
2025-03-05 22:01:42 +0000

    adds button cursor not allowed for button disabled state - v1

--- src/Components/CreateEventForm.tsx
+++ src/Components/CreateEventForm.tsx
@@ -38,7 +38,6 @@ const CreateEventForm = () => {
         "Missing API credentials. Please check your environment variables."
       );
       console.error("Error: Missing API token or organization ID.");
-      setDisable(false);
       return;
     }
     const currentTime = new Date();
@@ -47,14 +46,12 @@ const CreateEventForm = () => {
 
     if (selectedStartTime < currentTime) {
       alert("Start time cannot be in the past. Please select a future time.");
-      setDisable(false);
       return;
     }
     if (selectedStartTime > selectedEndTime) {
       alert(
         "Start time cannot be before end time. Please select appropprite start time."
       );
-      setDisable(false);
       return;
     }
 
@@ -160,7 +157,6 @@ const CreateEventForm = () => {
                   error.response?.data?.error_description || error.message
                 }`
               );
-              setDisable(false);
             } else {
               console.error("Unexpected error:", error);
               setMessage(
@@ -337,24 +333,28 @@ const CreateEventForm = () => {
                 <img src={formData.id} alt="Event Logo" />
               </div> */}
             </div>
-            {/* <div
-            // className="w-75 text-md rounded-md px-3 py-3 text-center focus:outline-none  shadow-sm focus:shadow justify-self-center mt-2 bg-white text-[#486570] hover:bg-[#d5f483] hover:text-[#486570] border transition-all duration-200 cursor-pointer"
-            // style={{
-            //   backgroundColor: "#d5f483",
-            //   color: "#486570",
-            //   fontSize: "20px",
-            //   textAlign: "center",
-            //   cursor: "pointer",
-            // }}
-            > */}
-            <button
-              className="w-75 text-md rounded-md px-3 py-3 text-center focus:outline-none  shadow-sm focus:shadow justify-self-center mt-2 bg-white text-[#486570] hover:bg-[#d5f483] hover:text-[#486570] border transition-all duration-200 cursor-pointer"
-              type="submit"
-              disabled={disable}
+            <div
+              className="mt-3 flex flex-col items-center justify-items-center w-full"
+              // style={{
+              //   backgroundColor: "#d5f483",
+              //   color: "#486570",
+              //   fontSize: "20px",
+              //   textAlign: "center",
+              //   cursor: "pointer",
+              // }}
             >
-              Submit
-            </button>
-            {/* </div> */}
+              <button
+                className={`text-md rounded-md px-3 py-3 text-center focus:outline-none  shadow-sm focus:shadow justify-self-center mt-2 bg-white text-[#486570] ${
+                  disable
+                    ? "cursor-not-allowed opacity-50"
+                    : " hover:bg-[#d5f483] hover:text-[#486570] border transition-all duration-200 cursor-pointer"
+                } `}
+                type="submit"
+                disabled={disable}
+              >
+                Submit
+              </button>
+            </div>
           </form>
         </div>
         <div>
