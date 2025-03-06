f54f95c47d2ed3fba229f5932656ad7b807db611
ruchikaroy <contactrroy.uk@gmail.com>
2025-03-06 12:02:14 +0000

    refactoring loading spinner create event form - v13

--- src/Components/CreateEventForm.tsx
+++ src/Components/CreateEventForm.tsx
@@ -356,14 +356,13 @@ const CreateEventForm = () => {
           </form>
         </div>
         <div>
-          {isLoading && (
+          {isLoading ? (
             <div className="d-flex justify-content-center align-items-center mt-4">
               <Spinner animation="border" variant="white" role="status">
                 <span className="visually-hidden">Loading...</span>
               </Spinner>
             </div>
-          )}
-          {!isLoading && (
+          ) : (
             <p
               className="font-thin"
               style={{ color: "#f4f4f4", fontSize: "20px", cursor: "pointer" }}
@@ -371,6 +370,14 @@ const CreateEventForm = () => {
               {message}
             </p>
           )}
+          {/* {!isLoading && (
+            <p
+              className="font-thin"
+              style={{ color: "#f4f4f4", fontSize: "20px", cursor: "pointer" }}
+            >
+              {message}
+            </p>
+          )} */}
           {/* {disable ? (
             <div className="d-flex justify-content-center align-items-center mt-4">
               <Spinner animation="border" variant="white" role="status">
